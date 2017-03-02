8969dcf6dc613eb539326bd0b5b37840fefbea02
Brendan Gordon <brendan.gordon@digital.gov.au>
2017-03-02 11:53:05 +1100

    Move all search js into search.js.liquid

--- _assets/js/search.js
+++
@@ -1,8 +0,0 @@
-//= require 'vendor/jquery-1.12.4.min'
-
-//= require 'vendor/lunr.min'
-//= require 'vendor/mustache'
-//= require 'vendor/date.format'
-//= require 'vendor/URI'
-//= require 'vendor/jquery.lunr.search'
-
